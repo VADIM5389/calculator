5ba418643d787ae5f63869371d67b1032a334a5a
VADIM5389 <78856791+VADIM5389@users.noreply.github.com>
2024-01-22 16:16:15 +0500

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1,num2, result = 0;char operation;
+            Console.WriteLine("Введите первое число: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Выберите операцию (+, -, *, /): ");
+            operation = Convert.ToChar(Console.ReadLine());
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+            }
+            Console.WriteLine($"Результат операции: {result}");
+            Console.ReadKey();
+        }
+    }
+}
